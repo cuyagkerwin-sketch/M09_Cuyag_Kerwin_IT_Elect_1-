336a49c1a7fbc9dc9ae8a95e303228d931b21593
Añasco_D <aasco@gmail.com>
2025-09-03 17:33:13 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,24 @@
+// App.js
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <CounterApp />
+      <ColorChangerApp />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
